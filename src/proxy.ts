3d26dab914e18615e4c13efd00393ecd8486eeb3
Andrew Boylan <andreweboylan@gmail.com>
2026-06-15 15:44:34 -0400

    update anger modal

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { ANGER_ACCESS_COOKIE, getAngerAccessToken } from "@/lib/anger-access";
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const password = process.env.ANGER_ACCESS_PASSWORD;
   const nextPath = `${request.nextUrl.pathname}${request.nextUrl.search}`;
   const passwordUrl = new URL("/anger-password", request.url);
